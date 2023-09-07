663b4e21704e0027da015947827fbebf17f41e0d
VijayReddy2903 <reddy.vijay2910@gmail.com>
2023-09-07 16:39:27 -0500

    Practice2

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "Practice-Instance" {
+  ami           = "ami-051f7e7f6c2f40dc1"
+  instance_type = "t2.micro"
+  key_name      = "Practice2"
+  tags = {
+    "Name" = "Practice2"
+  }
+}
